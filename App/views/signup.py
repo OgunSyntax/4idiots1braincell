017bdfb8de9a6a04116d2547f1f5b43e0368bcb4
OgunSyntax <jaysenbarry@gmail.com>
2025-04-17 15:03:27 -0400

    commit

--- App/views/signup.py
+++ App/views/signup.py
@@ -1,3 +1,4 @@
+
 from flask import Blueprint, render_template, jsonify, request, send_from_directory, flash, redirect, url_for
 from flask_jwt_extended import jwt_required, current_user as jwt_current_user
 
@@ -16,3 +17,5 @@ signup_views = Blueprint('signup_views', __name__, template_folder='../templates
 @signup_views.route('/signup', methods=['GET'])
 def signup_page():
     return render_template('signup.html')
+
+#sign
