5b74949e8303ccbf084fc81d002a728c14b1367c
OgunSyntax <jaysenbarry@gmail.com>
2025-04-17 15:04:23 -0400

    Commit

--- App/views/signup.py
+++ App/views/signup.py
@@ -17,5 +17,3 @@ signup_views = Blueprint('signup_views', __name__, template_folder='../templates
 @signup_views.route('/signup', methods=['GET'])
 def signup_page():
     return render_template('signup.html')
-
-#sign
